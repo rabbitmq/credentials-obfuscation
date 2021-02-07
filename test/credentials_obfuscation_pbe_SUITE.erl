3e864ef0d270061cb19d7abcc58ed7fed09d8db1
Dominic Morneau <dmorneau@gmail.com>
2021-02-07 19:05:18 +0900

    Ensures tests pass on macOS

--- test/credentials_obfuscation_pbe_SUITE.erl
+++ test/credentials_obfuscation_pbe_SUITE.erl
@@ -8,17 +8,21 @@
 -module(credentials_obfuscation_pbe_SUITE).
 -include_lib("common_test/include/ct.hrl").
 -compile(export_all).
- 
+
+%% This cipher is listed as supported on macOS, but doesn't actually work.
+%% OTP bug: https://bugs.erlang.org/browse/ERL-1478
+-define(SKIPPED_CIPHERS, [aes_ige256]).
+
 all() -> [
     encrypt_decrypt,
     encrypt_decrypt_charlist_value,
     encrypt_decrypt_term
 ].
- 
+
 encrypt_decrypt(_Config) ->
     %% Take all available block ciphers.
     Hashes = credentials_obfuscation_pbe:supported_hashes(),
-    Ciphers = credentials_obfuscation_pbe:supported_ciphers(),
+    Ciphers = credentials_obfuscation_pbe:supported_ciphers() -- ?SKIPPED_CIPHERS,
     %% For each cipher, try to encrypt and decrypt data sizes from 0 to 64 bytes
     %% with a random Secret.
     _ = [begin
@@ -36,7 +40,7 @@ encrypt_decrypt(_Config) ->
 
 encrypt_decrypt_charlist_value(_Config) ->
     Hashes = credentials_obfuscation_pbe:supported_hashes(),
-    Ciphers = credentials_obfuscation_pbe:supported_ciphers(),
+    Ciphers = credentials_obfuscation_pbe:supported_ciphers() -- ?SKIPPED_CIPHERS,
     _ = [begin
              Secret = crypto:strong_rand_bytes(16),
              Iterations = rand:uniform(100),
@@ -52,7 +56,7 @@ encrypt_decrypt_charlist_value(_Config) ->
 encrypt_decrypt_term(_Config) ->
     %% Take all available block ciphers.
     Hashes = credentials_obfuscation_pbe:supported_hashes(),
-    Ciphers = credentials_obfuscation_pbe:supported_ciphers(),
+    Ciphers = credentials_obfuscation_pbe:supported_ciphers() -- ?SKIPPED_CIPHERS,
     %% Different Erlang terms to try encrypting.
     DataSet = [
         10000,
@@ -74,4 +78,4 @@ encrypt_decrypt_term(_Config) ->
              Data = credentials_obfuscation_pbe:decrypt_term(C, H, Iterations, Secret, Enc)
          end || H <- Hashes, C <- Ciphers, Data <- DataSet],
     ok.
- 
+
